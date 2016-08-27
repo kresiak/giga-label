5be766e1011e059102c1c80ff6a820d23888dc88
Alexander Kvasz <akvasz@swisslex.ch>
2016-08-27 23:37:21 +0200

    fix for node error : doesn't work offline

--- WelcomeNewGuys/server.js
+++ WelcomeNewGuys/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 var db;
 
-var connectionUrl = 'mongodb://localhost:27017/giga';
+var connectionUrl = 'mongodb://127.0.0.1:27017/giga';
 
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(connectionUrl, function (err, database) {
